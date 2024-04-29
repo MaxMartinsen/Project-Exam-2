cc46f069f5cac6d9a90144c488b7bd4c35b697e2
Max Martinsen <max.martinsen@hotmail.com>
2024-04-29 15:40:32 +0200

    Created Dashboard

---
+++ src/components/Forms/VenuesForm.jsx
@@ -0,0 +1,13 @@
+function VenuesForm() {
+  return (
+    <section className="container px-4 mx-auto">
+      <div className="flex items-center gap-x-3">
+        <h2 className="text-lg font-medium text-gray-800 dark:text-white">
+          Create Venue
+        </h2>
+      </div>
+    </section>
+  );
+}
+
+export default VenuesForm;
--- src/pages/Manager/ManagerProfile.jsx
+++ src/pages/Manager/ManagerProfile.jsx
@@ -1,4 +1,16 @@
+import { useState } from 'react';
+import VenuesForm from '../../components/Forms/VenuesForm';
+import TablesProfiles from '../../components/Tables/TablesProfiles';
+import TablesVenues from '../../components/Tables/TablesVenues';
+
 function ManagerProfile() {
+  const [activeView, setActiveView] = useState('venue');
+
+  const getButtonClass = (view) => {
+    return `inline-flex items-center h-12 px-4 py-2 text-sm text-center border-gray-300 sm:text-base rounded-t-md whitespace-nowrap focus:outline-none ${
+      activeView === view ? 'border border-b-0' : 'bg-transparent border-b'
+    }`;
+  };
   return (
     <>
       <section className="bg-white dark:bg-gray-900">
@@ -12,20 +24,33 @@ function ManagerProfile() {
           </p>
 
           <div className="flex overflow-x-auto whitespace-nowrap mt-4">
-            <button className="inline-flex items-center h-12 px-4 py-2 text-sm text-center text-gray-700 border border-b-0 border-gray-300 sm:text-base dark:border-gray-500 rounded-t-md dark:text-white whitespace-nowrap focus:outline-none">
+            <button
+              onClick={() => setActiveView('venue')}
+              className={getButtonClass('venue')}
+            >
               Venue
             </button>
 
-            <button className="inline-flex items-center h-12 px-4 py-2 text-sm text-center text-gray-700 bg-transparent border-b border-gray-300 sm:text-base dark:border-gray-500 dark:text-white whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400 dark:hover:border-gray-300">
+            <button
+              onClick={() => setActiveView('create')}
+              className={getButtonClass('create')}
+            >
               Create
             </button>
 
-            <button className="inline-flex items-center h-12 px-4 py-2 text-sm text-center text-gray-700 bg-transparent border-b border-gray-300 sm:text-base dark:border-gray-500 dark:text-white whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400 dark:hover:border-gray-300">
+            <button
+              onClick={() => setActiveView('profiles')}
+              className={getButtonClass('profiles')}
+            >
               All profiles
             </button>
             <div className="inline-flex items-center w-full h-12 px-4 py-2 text-sm text-center text-gray-700 bg-transparent border-b border-gray-300 sm:text-base whitespace-nowrap cursor-base focus:outline-none"></div>
           </div>
         </div>
+
+        {activeView === 'venue' && <TablesVenues />}
+        {activeView === 'create' && <VenuesForm />}
+        {activeView === 'profiles' && <TablesProfiles />}
       </section>
     </>
   );
