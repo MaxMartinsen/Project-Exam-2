be35ebbc2a3c5e28aca5755c6990819a06bbe624
Max Martinsen <max.martinsen@hotmail.com>
2024-04-30 14:10:02 +0200

    Update SettingsProfile.jsx

--- src/components/Settings/SettingsProfile.jsx
+++ src/components/Settings/SettingsProfile.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateProfile } from '../../features/profile/profileSlice';
 import { updateUser } from '../../features/user/userSlice';
-import UpdateAvatar from '../Modal/UpdateAvatar';
+import UpdateAvatar from '../Modal/UpdateAvatar.jsx';
 import DEFAULT_AVATAR from '../../assets/image/default-profile.png';
 import { DEFAULT_AVATAR_URL } from '../../utils/constans';
 
