76524c3da62d8d6c6e8ad1cd5947e6874c23bbcc
Max Martinsen <max.martinsen@hotmail.com>
2024-05-15 21:01:13 +0200

    Add Jest setup and test for App component. Update README with test run description.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,18 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, node: true },
+  env: { browser: true, es2020: true, node: 'current', 'jest/globals': true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jest/recommended',
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', 'prettier'],
+  plugins: ['react-refresh', 'prettier', 'jest'],
   rules: {
     'react/prop-types': 'off',
     'prettier/prettier': 'error',
